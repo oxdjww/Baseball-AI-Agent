c16c5375f1d2f72a3d7a707cff5ba4d8bbd5539d
oxdjww <oxdjww@gmail.com>
2025-07-15 23:56:06 +0900

    simple batch poc

--- src/main/java/com/kbank/baa/BaaApplication.java
+++ src/main/java/com/kbank/baa/BaaApplication.java
@@ -2,12 +2,12 @@ package com.kbank.baa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BaaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BaaApplication.class, args);
 	}
-
 }
