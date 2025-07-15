a20d04b4322d274c10405887b42ae030e492a87c
oxdjww <oxdjww@gmail.com>
2025-07-15 23:23:45 +0900

    init

---
+++ src/main/java/com/kbank/baa/BaaApplication.java
@@ -0,0 +1,13 @@
+package com.kbank.baa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbank/baa/BaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbank.baa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
