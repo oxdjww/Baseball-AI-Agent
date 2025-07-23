4e0426b164fd79f7235117db9228f45227022c0c
oxdjww <oxdjww@gmail.com>
2025-07-23 19:58:28 +0900

    Chore: 텔레그램 메시지 전송 시 로깅 적용

--- backend/src/main/java/com/kbank/baa/telegram/TelegramService.java
+++ backend/src/main/java/com/kbank/baa/telegram/TelegramService.java
@@ -1,12 +1,14 @@
 package com.kbank.baa.telegram;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class TelegramService {
     private final TelegramProperties props;
@@ -15,6 +17,8 @@ public class TelegramService {
     // 멤버별 chatId로 전송
     public void sendMessage(String chatId, String text) {
         String url = props.getApiUrl() + "sendMessage";
+        log.info("##### Telegram으로 메시지 전송 시도: url={}, chatId={}, text={}", url, chatId, text);
+
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("chat_id", chatId);
         body.add("text", text);
