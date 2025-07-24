47fed9687d42536e2804a0bc46283cce51878b5c
oxdjww <oxdjww@gmail.com>
2025-07-24 21:48:39 +0900

    Fix: 타임존 문제로 스케줄링되지 않던 우천알림 스케줄링 로직을 수정

--- backend/src/main/java/com/kbank/baa/BaaApplication.java
+++ backend/src/main/java/com/kbank/baa/BaaApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class BaaApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(BaaApplication.class, args);
     }
 }
